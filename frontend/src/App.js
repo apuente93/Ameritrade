f4a8ddd6def906e0e9a5eefa4f9ccce3e1ced043
apuente93 <alejandropuentetelle@gmail.com>
2022-04-09 23:59:25 -0500

    Connecting frontend to backend api

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+const React = require('react');
+const ReactDOM = require('react-dom'); 
+const client = require('./client'); 
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+	constructor(props) {
+		super(props);
+		this.state = {stocks: []};
+	}
+
+	componentDidMount() { 
+		client({method: 'GET', path: '/api/api/v1/stock'}).done(response => {
+			this.setState({stocks: response.entity._embedded.stocks});
+		});
+	}
+
+	render() {
+		return (
+			<StockList stocks={this.state.stocks}/>
+		)
+	}
 }
 
-export default App;
+class StockList extends React.Component{
+	render() {
+		const stocks = this.props.stocks.map(stock =>
+			<Stock key={stock._links.self.href} stock={stock}/>
+		);
+		return (
+			<table>
+				<tbody>
+					<tr>
+						<th>First Name</th>
+						<th>Last Name</th>
+						<th>Description</th>
+					</tr>
+					{stocks}
+				</tbody>
+			</table>
+		)
+	}
+}
+
+class Stock extends React.Component{
+	render() {
+		return (
+			<tr>
+				<td>{this.props.stock.name}</td>
+				<td>{this.props.stock.ticker}</td>
+				<td>{this.props.stock.description}</td>
+			</tr>
+		)
+	}
+}
+
+ReactDOM.render(
+	<App />,
+	document.getElementById('react')
+)
